540ad386ff2ea5c246eae13423c70853674561a6
DMarwan <marwan.professionnel@gmail.com>
2019-08-05 02:52:07 +0200

    AutoLink first commit 08/05/2019

---
+++ calltracker.py
@@ -0,0 +1,8 @@
+def calltracker(func):
+    import functools
+    @functools.wraps(func)
+    def wrapper(*args):
+        wrapper.has_been_called = True
+        return func(*args)
+    wrapper.has_been_called = False
+    return wrapper
